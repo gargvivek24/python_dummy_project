8c7ca2cfd15e3f3cde8bca2c39458d0527e4791d
Vivek <vivek.garg@complereinfosystem.com>
2022-02-23 16:40:41 +0530

    dice game playing from other file

---
+++ triggergame.py
@@ -0,0 +1,11 @@
+import time
+start = time.time()
+start= round(start, 2)
+print("Start time is ", start)
+exec(open("dicegame.py").read())
+time.sleep(5)
+end = time.time()
+end=round(end, 2)
+print("End time is ", end )
+execution_time = round(end-start, 2)
+print("Time taken by program to execute ", execution_time, " Seconds")
