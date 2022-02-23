45cefc60b57c4e10ebb69589ad261c6c910090be
Vivek <vivek.garg@complereinfosystem.com>
2022-02-23 16:49:55 +0530

    dummy

--- triggergame.py
+++ triggergame.py
@@ -1,11 +1,15 @@
-import time
-start = time.time()
-start= round(start, 2)
-print("Start time is ", start)
-exec(open("dicegame.py").read())
-time.sleep(5)
-end = time.time()
-end=round(end, 2)
-print("End time is ", end )
-execution_time = round(end-start, 2)
-print("Time taken by program to execute ", execution_time, " Seconds")
+game=int(input("Enter which game you wish to play 1 for diceguess 2 for rock paper scissors"))
+if game==1:
+    import time
+    start = time.time()
+    start= round(start, 2)
+    print("Start time is ", start)
+    exec(open("dicegame.py").read())
+    time.sleep(5)
+    end = time.time()
+    end=round(end, 2)
+    print("End time is ", end )
+    execution_time = round(end-start, 2)
+    print("Time taken by program to execute ", execution_time, " Seconds")
+elif game==2:
+    exec(open("rock_paper_scissors.py").read())
