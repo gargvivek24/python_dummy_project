2ded285af92ca50bff9510f634bf20054d0daa9c
Vivek <vivek.garg@complereinfosystem.com>
2022-02-24 13:04:07 +0530

    Getting the config file directory and than moving the file we are working on to the directory in which config file is present

--- try_new.py
+++ try_new.py
@@ -35,29 +35,3 @@ shutil.copy(current_file2, head2)
 
 
 
-#
-#
-# config = configparser.ConfigParser()
-# a=config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
-# print(a)
-#
-# def listToString(s):
-#     # initialize an empty string
-#     str1 = ""
-#
-#     # return string
-#     return (str1.join(s))
-# b=listToString(a)
-# head1,tail1= os.path.split(b)
-# print(head1)
-# head2=head1+'\\'
-# print(head2)
-# print(tail1)
-# current_file=os.path.basename(__file__)
-# print(current_file)
-# completeName = os.path.join(head2, current_file)
-# print(completeName)
-# file = open(completeName,'a')
-# file.write('print("Blah Blah")')
-# file.close()
-# print("Blah Blah")
